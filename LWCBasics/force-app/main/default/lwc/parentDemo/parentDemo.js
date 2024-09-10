9965f85fcf77d4a1e7ec7df9e8e247cfa4937b14
LearningBox <lnpandey14@gmail.com>
2024-09-11 00:28:04 +0530

    Added track decorator for array in parent

--- LWCBasics/force-app/main/default/lwc/parentDemo/parentDemo.js
+++ LWCBasics/force-app/main/default/lwc/parentDemo/parentDemo.js
@@ -1,5 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class ParentDemo extends LightningElement {
-    
+    incrementValue=3;
+    nonReactiveArray=[1,2,3];
+    @track reactiveArray=[1,2,3];
+
+    updateReactiveArray(event){
+        this.incrementValue++;
+this.reactiveArray.push(this.incrementValue);
+    }
+    updateNonReactiveArray(event){
+        this.incrementValue++;
+        this.nonReactiveArray.push(this.incrementValue);
+            }
 }
