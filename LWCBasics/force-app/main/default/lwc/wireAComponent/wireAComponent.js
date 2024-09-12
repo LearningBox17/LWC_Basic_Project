ef022b270289a52b66426975fa59caa0328515fa
LearningBox <lnpandey14@gmail.com>
2024-09-12 15:18:06 +0530

    Wiring Apex Method for combobox and datatable

---
+++ LWCBasics/force-app/main/default/lwc/wireAComponent/wireAComponent.js
@@ -0,0 +1,29 @@
+import { LightningElement, wire } from 'lwc';
+import getwireddata from '@salesforce/apex/WiredClass.getwireddata';
+
+import getAccData from '@salesforce/apex/WiredClass.getAccData';
+export default class WireAComponent extends LightningElement {
+accId='';
+accData=[];
+error;
+    @wire(getAccData) wiretest({error, data}){
+        this.accData=[];
+        if(data){
+            console.log('display Accounts', data);
+           
+                this.accData.push({label: 'Select Any Account', value:''});
+                data.forEach(acc=>{
+                    this.accData.push({label:acc.Name, value: acc.Id});
+                });
+            
+        }else if(error){
+            this.error=error;
+        }
+        }
+onSelection(event){
+    this.accId = event.target.value;
+}
+    @wire(getwireddata,{accId:'$accId'}) wiredData;
+
+    columns=[{label:'Name', type:'text', fieldName: 'Name'}];
+}
